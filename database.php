55d2bcdce9c952ad46134a1fe8e7850a0e379c52
matikd41 <mdamania2@gmail.com>
2025-04-14 21:00:31 -0400

    connected mysql to php

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "professor_rating_db";
+    $connection = "";
+
+    try{
+        $connection = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    }
+
+    catch(mysqli_sql_exception){
+        echo("Could not connect");
+    }
+    if($connection){
+        echo("You are connected");
+    }
+?>
