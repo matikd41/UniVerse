e678d9dece6b0a22983675d2bfa41f43b8c52abd
nhdion <ndionisio@oakland.edu>
2025-04-19 12:54:18 -0400

    added the ability to delete posts, tweaked other things

--- profile.php
+++ profile.php
@@ -7,9 +7,15 @@ $database = "universe_db";
 
 $conn = new mysqli($server, $user, $pass, $database);
 
-$profile_meta = "SELECT * FROM profile_meta";//profile metadata
+$user_id = (int) $_GET["id"];
 
-$post = "SELECT * FROM post";//user posts
+if($user_id == 1) {//temporary; id of 1 will be logged in user's profile
+	$user_id = 2;//temporary; will change to logged in user's id
+}
+
+$profile_meta = "SELECT * FROM profile_meta WHERE ID = $user_id";//profile metadata
+
+$post = "SELECT * FROM post WHERE user_id = $user_id";//user posts
 
 $result_profile = $conn->query($profile_meta);
 $row_profile = $result_profile->fetch_assoc();
@@ -38,10 +44,12 @@ $result_post = $conn->query($post);
 		<div class="container">
 			<div class="content-left">
 				<div class="post">
+					<!--- div for creating new posts --->
 					<label for="newPost">Post something new!</label>
 					<br>
 					<form action="newPost.php" method="post">
 					<input style="width:80%;"type="text" id="newPost" name="newPost">
+					<input type="hidden" name="id" id="id" value="<?php echo $user_id; ?>" />
 					<button type="submit">Post</button>
 					</form>
 				</div>
@@ -49,6 +57,11 @@ $result_post = $conn->query($post);
 				<div class="post">
 					<img src="images/defaultPfp.jpg" alt="User profile picture">
 					<p class="post-info" style="display:inline;"><?php echo $row_post["name"] ?></p>
+					<form action="deletePost.php" method="post">
+					<input type="hidden" name="post-id" id="post-id" value="<?php echo $row_post["post_id"] ?>" />
+					<input type="hidden" name="user-id" id="user-id" value="<?php echo $user_id ?>" />
+					<button type="submit" class="del">Delete Post</button>
+					</form>
 					<p class="post-text"><?php echo $row_post["post"] ?></p>
 					<p class="post-info" style="text-align:right;"><?php echo "Posted " . $row_post["date"] ?></p>
 				</div>
